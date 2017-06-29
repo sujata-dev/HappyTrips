fd4612495b75760a541e38f12f8d49ff86892997
Sujata Dev <sujatadev97@gmail.com>
2017-06-29 20:28:17 +0530

    database AirbusRecord modified

--- src/flightrecordDB.sql
+++ src/flightrecordDB.sql
@@ -4,14 +4,21 @@ use flightrecord;
 
 create table AirbusRecord
 (
-    airbusno        int not null,
-    departure       int not null,
-    arrival         int not null,
-    businessfare    int not null,
-    economicalfare  int not null,
+    airbusno        int,
+    flightname      varchar(255),
+    journeydate     date,
+    departure       varchar(255),
+    departuretime   varchar(255),
+    arrival         varchar(255),
+    arrivaltime     varchar(255),
+    businessfare    int,
+    economicalfare  int,
 
     primary key(airbusno)
 );
+
+insert into AirbusRecord
+values(1001, 'SpiceJet', '2017-02-27', 'Hyderabad', '20:55', 'Raipur', '23:05', 5000, 2000);
 
 
 create table aircodes
@@ -24,4 +31,34 @@ create table aircodes
 );
 
 insert into aircodes
-values('0003', 'Hyderabad', 'Telangana', 'Rajiv Gandhi International Airport');
+values('0001', 'Port Blair', 'Andaman & Nicobar Islands',
+                                    'Veer Savarkar International Airport');
+insert into aircodes
+values('0003', 'Hyderabad', 'Telangana',
+                                    'Rajiv Gandhi International Airport');
+insert into aircodes
+values('0004', 'Tirupati', 'Andhra Pradesh',
+                                    'Tirupati Airport');
+insert into aircodes
+values('0005', 'Vijayawada', 'Andhra Pradesh',
+                                    'Vijayawada International Airport');
+insert into aircodes
+values('0006', 'Visakhapatnam', 'Andhra Pradesh',
+                                    'Visakhapatnam Airport');
+insert into aircodes
+values('0008', 'Itanagar', 'Arunachal Pradesh',
+                                    'Itanagar Airport');
+insert into aircodes
+values('0014', 'Silchar', 'Assam',
+                                    'Silchar Airport');
+insert into aircodes
+values('0016', 'Gaya', 'Bihar',
+                                    'Gaya International Airport');
+insert into aircodes
+values('0022', 'Raipur', 'Chhattisgarh',
+                                    'Swami Vivekananda Airport');
+insert into aircodes
+values('0025', 'New Delhi', 'New Delhi',
+                                    'Indira Gandhi International Airport');
+
+
