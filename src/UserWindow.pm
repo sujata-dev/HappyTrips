efc774c830ac1a54a53a72c1bca0ce293b10a0b0
Sujata Dev <sujatadev97@gmail.com>
2017-07-02 12:42:11 +0530

    added booking termination feature

--- src/UserWindow.pm
+++ src/UserWindow.pm
@@ -7,7 +7,15 @@ use warnings;
 use DBI;
 use Tk;
 
-our $user;
+my $driver = "mysql";
+my $database = "CustomerRecord";
+my $dsn = "DBI:mysql:CustomerRecord";
+my $userid = "root";
+my $pwd = "root";
+my $dbh = DBI -> connect($dsn, $userid, $pwd ) or die $DBI::errstr;
+
+my ($eticketno, $fare, $cancellationfare, $refund);
+our ($user, $cancelbooking);
 
 
 sub UserWindow
@@ -45,24 +53,79 @@ sub UserWindow
 
     $user -> Button
     (
-        -text => 'Change a booking',
-        -font => 'Roman 14 bold',
-        -command =>
-            sub
-            {
-
-            },
-        -background => 'goldenrod2'
-    ) -> pack(-side => 'top', -pady => 10);
-
-    $user -> Button
-    (
         -text => 'Terminate a booking',
         -font => 'Roman 14 bold',
         -command =>
             sub
             {
+                $cancelbooking = MainWindow -> new(-background => 'DarkSlateGray');
+                $cancelbooking -> title("Add flight Window");
 
+                $cancelbooking -> Label
+                (
+                    -text => 'Enter eticket number to cancel your booking',
+                    -foreground => 'white',
+                    -background => 'DarkSlateGray',
+                    -font => 'Georgia 12 normal',
+                ) -> pack(-side => 'top', -pady => 2);
+
+                $cancelbooking -> Entry
+                (
+                    -textvariable => \$eticketno,
+                    -background => 'SlateGray',
+                    -font => 'Georgia 10 bold',
+                ) -> pack(-side => 'top', -pady => 2);
+
+                $cancelbooking -> Button
+                (
+                    -text => 'Cancel Booking',
+                    -font => 'Roman 14 bold',
+                    -command =>
+                        sub
+                        {
+                            my $sth = $dbh -> prepare("select fare from Airticket
+                                            where eticketno = \'$eticketno\'");
+                            $sth -> execute() or die $DBI::errstr;
+
+                            while (my $row = $sth -> fetchrow_array())
+                            {
+                                $fare = $row;
+                            }
+
+                            $cancellationfare = 0.25 * $fare;
+                            $refund = $fare - $cancellationfare;
+
+                            $sth = $dbh -> prepare("delete from Airticket
+                                            where eticketno = \'$eticketno\'");
+                            $sth -> execute() or die $DBI::errstr;
+
+                            $cancelbooking -> Label
+                            (
+                                -text => 'Booking Cancelled',
+                                -foreground => 'white',
+                                -background => 'DarkSlateGray',
+                                -font => 'Georgia 14 bold',
+                            ) -> pack(-side => 'top', -pady => 15);
+
+                            $cancelbooking -> Label
+                            (
+                                -text => 'Cancellation Charges: '.
+                                                        $cancellationfare,
+                                -foreground => 'white',
+                                -background => 'DarkSlateGray',
+                                -font => 'Georgia 12 bold',
+                            ) -> pack(-side => 'top', -pady => 2);
+
+                            $cancelbooking -> Label
+                            (
+                                -text => 'Refundable Amount: '.$refund,
+                                -foreground => 'white',
+                                -background => 'DarkSlateGray',
+                                -font => 'Georgia 12 bold',
+                            ) -> pack(-side => 'top', -pady => 2);
+                        },
+                    -background => 'goldenrod2'
+                ) -> pack(-side => 'top', -pady => 15);
             },
         -background => 'goldenrod2'
     ) -> pack(-side => 'top', -pady => 10);
