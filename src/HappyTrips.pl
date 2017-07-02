da162ac34d7d56ac6393c3e1114ebd39b3604bc2
Sujata Dev <sujatadev97@gmail.com>
2017-07-02 12:35:20 +0530

    added StaffWindow

--- src/HappyTrips.pl
+++ src/HappyTrips.pl
@@ -12,15 +12,18 @@ use BookFlight;
 use UserWindow;
 use CustomerLoginWindow;
 use StaffLoginWindow;
+use StaffWindow;
 use CustomerRegistrationSuccess;
 use CustomerRegistrationFailure;
 
 
 
 StaffLoginWindow::StaffLoginWindow();
+StaffWindow::StaffWindow();
 BookFlight::BookFlight();
 UserWindow::UserWindow();
 CustomerLoginWindow::CustomerLoginWindow();
 CustomerRegistrationFailure::CustomerRegistrationFailure();
 CustomerRegistrationSuccess::CustomerRegistrationSuccess();
 SignUpWindow::SignUpWindow();
+
